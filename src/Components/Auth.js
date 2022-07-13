eab178afe0f27245278eefb6811086ab4359d4d2
eugenia-stefan <stefaneugeniagabriela@gmail.com>
2022-07-13 18:57:44 +0300

    login-page

---
+++ src/Components/Auth.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Navbar from "./Navbar.js";
+
+export default function Auth() {
+  return (
+    <>
+      <Navbar />
+      <main className="auth-main">
+        <div className="form-signin w-100 m-auto">
+          <form>
+            <h1 className="h3 mb-3 fw-normal text-center">Sign in</h1>
+
+            <div className="form-floating">
+              <input
+                type="email"
+                className="form-control"
+                id="floatingInput"
+                placeholder="name@example.com"
+              />
+              <label htmlFor="floatingInput">Email address</label>
+            </div>
+            <div className="form-floating">
+              <input
+                type="password"
+                className="form-control"
+                id="floatingPassword"
+                placeholder="Password"
+              />
+              <label htmlFor="floatingPassword">Password</label>
+            </div>
+
+            <div className="checkbox mb-3">
+              <label>
+                <input type="checkbox" value="remember-me" /> Remember me
+              </label>
+            </div>
+            <button className="w-100 btn btn-lg btn-primary" type="submit">
+              Sign in
+            </button>
+            {/* <p className="mt-5 mb-3 text-muted">&copy; 2017–2022</p> */}
+          </form>
+        </div>
+      </main>
+    </>
+  );
+}
