720cf274204452e3304e01e8355ac16cfaa9dc32
eugenia-stefan <stefaneugeniagabriela@gmail.com>
2022-07-14 01:13:19 +0300

    registration-pg-added

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "./Components/Auth";
+import Registration from "./Components/Registration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -13,6 +14,7 @@ root.render(
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/sign-in" element={<Auth />} />
+      <Route path="/sign-up" element={<Registration />} />
     </Routes>
   </BrowserRouter>
 );
